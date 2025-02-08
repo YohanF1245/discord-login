8d6018a9887be1d322a3d8ed0cf9fb289a24c2e9
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2025-02-08 15:42:34 +0100

    style(front): add Tailwind and PostCSS configuration
    - Configure TailwindCSS\n- Add Skeleton UI plugin\n- Configure PostCSS\n- Add form plugins

---
+++ front/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')
+  ],
+  darkMode: 'class'
+}; 
